d8b4522712c202d39df133c5b663627e5aafb088
Leonardo Oliveira <159083640+leonardo-ods@users.noreply.github.com>
2025-10-30 20:59:35 -0300

    Update textarea styles for light and dark modes

--- IncluIA.py
+++ IncluIA.py
@@ -21,13 +21,13 @@ st.set_page_config(
 # --- CSS CUSTOMIZADO PARA MODO CLARO E ESCURO ---
 st.markdown("""
     <style>
-    textarea[disabled] {
+    textarea {
         -webkit-text-fill-color: #2e3136;
         color: #2e3136;
         background-color: #f0f2f6;
     }
 
-    body[data-theme="dark"] textarea[disabled] {
+    body[data-theme="dark"] textarea {
         -webkit-text-fill-color: #fafafa;
         color: #fafafa;
         background-color: #1c1f2b;
